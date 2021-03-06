c8558a7fc37dde69a013b3fbca78e534a55d85f6
Blake Enloe <blakewenloe@yahoo.com>
2021-03-05 18:16:29 -0600

    finished initial bot setup

--- habit.js
+++ habit.js
@@ -1,17 +1,23 @@
 const { CommandoClient } = require("discord.js-commando");
 const path = require("path");
 const dotenv = require("dotenv").config();
+const client_id = process.env.CLIENT_ID;
 
 const client = new CommandoClient({
   commandPrefix: "?",
   owner: process.env.BOT_OWNER_ID,
+  invite: `https://discord.com/oauth2/authorize?client_id=${client_id}&scope=bot&permissions=8`,
 });
-client.on("ready", async () => {
-  client.registry
-    .registerDefaults()
 
-    // Registers all of your commands in the ./commands/ directory
-    .registerCommandsIn(path.join(__dirname, "commands"));
+client.registry
+  .registerDefaults()
+  // Registers all of your commands in the ./commands/ directory
+  .registerCommandsIn(path.join(__dirname, "commands"));
+
+client.once("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
 });
+
+client.on("error", console.error);
 
 client.login(process.env.BOT_TOKEN);
